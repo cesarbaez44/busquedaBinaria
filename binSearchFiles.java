53256a452941180bf97e69714a72175f19955f75
Cesar <cesarbaezmoya06@gmail.com>
2025-11-20 22:40:42 -0600

    implementar metodo binarySearch

--- binSearchFiles.java
+++ binSearchFiles.java
@@ -48,5 +48,19 @@ public class binSearchFiles {
         }
         return array;
     }
+     // realiza busqueda binaria en el  arreglo ordenado ascendentemente.
+
+    public int binarySearch(int[] arr, int target){
+        if (arr == null || arr.length == 0) return -1;
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right){
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) return mid;
+            else if (arr[mid] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        return -1;
+    }
 
 }
